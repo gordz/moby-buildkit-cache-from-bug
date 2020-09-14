20206f1b809a6af822b405a122f0ca0e07c5f3cd
Graham Gatus <ggatus@atlassian.com>
2020-09-14 16:22:09 +1000

    Fixing example.

--- test.sh
+++ test.sh
@@ -3,8 +3,8 @@
 set -e
 
 
-BASE_IMAGE="docker.atl-paas.net/ggatus/parent:101"
-CHILD_IMAGE="docker.atl-paas.net/ggatus/child:101"
+BASE_IMAGE="docker.atl-paas.net/ggatus/parent:latest"
+CHILD_IMAGE="docker.atl-paas.net/ggatus/child:latest"
 
 # clean up
 ./cleanup.sh
@@ -28,7 +28,6 @@ docker build \
     --build-arg base="$BASE_IMAGE" \
     --build-arg BUILDKIT_INLINE_CACHE=1 \
     --secret id=ssh_key,src=$(pwd)/secret1.txt \
-    --secret id=npmrc,src=$(pwd)/secret2.txt \
     -t ${CHILD_IMAGE} \
     .
 
@@ -51,7 +50,6 @@ docker build \
     --build-arg base="${BASE_IMAGE}" \
     --cache-from ${CHILD_IMAGE}  \
     --secret id=ssh_key,src=$(pwd)/secret1.txt \
-    --secret id=npmrc,src=$(pwd)/secret2.txt \
     -t ${CHILD_IMAGE} \
     .
 
